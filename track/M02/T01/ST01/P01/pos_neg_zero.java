761e386d01ef9edaf1ef95d34093b9eee3703afb
ruchitajadhav11 <ruchitajadhav112004@gmail.com>
2026-08-04 12:39:16 +0530

    num is positive ,negative or zero

---
+++ track/M02/T01/ST01/P01/pos_neg_zero.java
@@ -0,0 +1,15 @@
+
+public class pos_neg_zero {
+
+    public static void main(String[] args) {
+        int num = 25;
+        if (num > 0) {
+            System.out.println("positive");
+        } else if (num < 0) {
+            System.out.println("negative");
+        } else {
+            System.out.println("zero");
+        }
+    }
+
+}
