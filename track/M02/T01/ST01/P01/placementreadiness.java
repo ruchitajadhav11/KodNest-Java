471e57726c8fc6c0d0e044655494727c3010a31b
ruchitajadhav11 <ruchitajadhav112004@gmail.com>
2026-08-04 15:41:45 +0530

    placement readiness decision

---
+++ track/M02/T01/ST01/P01/placementreadiness.java
@@ -0,0 +1,23 @@
+
+public class placementreadiness {
+
+    public static void main(String[] args) {
+        int marks = 72;
+        int attendance = 80;
+        boolean activeBacklog = false;
+        boolean projectCompleted = true;
+        int mockInterviewScore = 65;
+
+        boolean academicEligibility = marks >= 60 && attendance >= 75 && !activeBacklog;
+
+        if (!academicEligibility) {
+            System.out.println("Improve Academic Eligibility");
+        } else if (!projectCompleted) {
+            System.out.println("Complete the Project");
+        } else if (mockInterviewScore < 70) {
+            System.out.println("Improve Interview Skills");
+        } else {
+            System.out.println("Placement Ready");
+        }
+    }
+}
