edd2bc69f5d8ebcb74eb5460e6edfccff6a7c1cd
ruchitajadhav11 <ruchitajadhav112004@gmail.com>
2026-08-18 11:23:57 +0530

    minimum  elemet in array

---
+++ track/arrays/mininArray.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class mininArray {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int arr[] = new int[5];
+        System.out.println("Enter 5 numbers: ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Array: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+        int min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        System.out.println("Minimum element in array: " + min);
+    }
+}
